9cb3dd406ed1e939f753838d32bd419bab1700ba
Giovanni Crabi <crabigiovanni@gmail.com>
2022-06-02 20:52:46 +0100

    configured users

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,17 +1,42 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
 import Link from "next/link";
+import { useEffect } from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
+
+import { useQuery } from 'react-query';
 
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination/";
 import { SideBar } from "../../components/Sidebar";
 
 export default function UserList() {
+  const { data, isLoading, error } = useQuery('users', async () => {
+    const response = await fetch('http://localhost:3000/api/users/')
+    const data = await response.json()
+
+    const users = data.users.map(user => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        created_at: new Date(user.created_at).toLocaleDateString('pt-br', {
+          day: '2-digit',
+          month:'long',
+          year: 'numeric'
+        })
+      }
+    });
+    return users;
+  });
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true
-  }
-)
+  })
+
+  useEffect(() => {
+
+  }, [])
 
   return (
     <Box>
@@ -24,86 +49,62 @@ export default function UserList() {
           <Flex mb='8' justify='space-between' align='center'>
             <Heading size='lg' fontWeight='normal'> Users </Heading>
 
-          <Link href='/users/create'>
-            <Button
+            <Link href='/users/create'>
+              <Button
                 as='a'
                 size='sm'
                 fontSize='sm'
                 colorScheme='pink'
                 cursor='crosshair'
-                leftIcon={<Icon as={RiAddLine} fontSize='20'/>}
+                leftIcon={<Icon as={RiAddLine} fontSize='20' />}
               >
-                New create 
+                New create
 
               </Button>
-          </Link>
-            
+            </Link>
           </Flex>
 
-          <Table colorScheme='whiteAlpha'>
-            <Thead>
-              <Tr>
-                <Th px='6' color='gray.300' width='8'>
-                </Th>
-                <Th>"Users</Th>
-               { isWideVersion && <Th>Registration data</Th> }
-                <Th width='8'></Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={['4', '4','6']}>
-                  <Checkbox colorScheme='pink'/>
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight='bold'>Giovanni</Text>
-                    <Text fontSize='sm' color='gray.300'>crabi@gmail.com</Text>
-                  </Box>
-                </Td>
-               { isWideVersion && <Td>24 de Abril de 2022</Td> }
-                <Td>
-                  <Button
-                    as='a'
-                    size='sm'
-                    fontSize='sm'
-                    colorScheme='purple'
-                    cursor='pointer'
-                    leftIcon={<Icon as={RiPencilLine} fontSize='16'/>}
-                  >
-                    { isWideVersion ? 'Edit' : '' }
-                  </Button>
-                </Td>
-              </Tr>
+          {isLoading ? (
+            <Flex justify='center'>
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify='center'>
+              <Text>Error at users's request</Text>
+            </Flex>
+          ) : (
+            <Table colorScheme='whiteAlpha'>
+              <Thead>
+                <Tr>
+                  <Th px='6' color='gray.300' width='8'>
+                  </Th>
+                  <Th>"Users</Th>
+                  {isWideVersion && <Th>Registration data</Th>}
+                  <Th width='8'></Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {data.map(user => {
+                  return (
+                      <Tr key={user.id}>
+                        <Td px={['4', '4', '6']}>
+                          <Checkbox colorScheme='pink' />
+                        </Td>
+                        <Td>
+                          <Box>
+                            <Text fontWeight='bold'>{user.name}</Text>
+                            <Text fontSize='sm' color='gray.300'>{user.email}</Text>
+                          </Box>
+                        </Td>
+                       {isWideVersion && <Td> {user.created_at} </Td>} 
+                    </Tr>
+                  )
+                })}
+              </Tbody>
+            </Table>
+          )}
 
-              <Tr>
-                <Td px={['4', '4','6']}>
-                  <Checkbox colorScheme='pink'/>
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight='bold'>Giovanni</Text>
-                    <Text fontSize='sm' color='gray.300'>crabi@gmail.com</Text>
-                  </Box>
-                </Td>
-               { isWideVersion && <Td>24 de Abril de 2022</Td> }
-                <Td>
-                  <Button
-                    as='a'
-                    size='sm'
-                    fontSize='sm'
-                    colorScheme='purple'
-                    cursor='pointer'
-                    leftIcon={ <Icon as={RiPencilLine} fontSize='16'/>}
-                  >
-                    { isWideVersion ? 'Edit' : '' }
-                  </Button>
-                </Td>
-              </Tr>
-            </Tbody>
-          </Table>
-
-        <Pagination />
+          <Pagination />
 
         </Box>
       </Flex>
--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,15 +1,35 @@
-import { createServer, Model } from 'miragejs';
+import { createServer, Factory, Model } from 'miragejs';
+
+import { faker } from '@faker-js/faker';
 
 type User = {
   name: string;
   email: string;
-  create_at:string;
+  created_at:string;
 }
 
 export function makeServer() {
   const server = createServer({
     models: {
       user: Model.extend<Partial<User>>({})
+    },
+
+    factories: {
+      user: Factory.extend({
+        name(i: number) {
+          return `User ${i + 1}`
+        },
+        email() {
+          return faker.internet.email().toLocaleLowerCase();
+        },
+        created_at() {
+          return faker.date.recent(10)
+        },
+      })
+    },
+
+    seeds(server) {
+      server.createList('user', 10)
     },
     routes() {
       this.namespace = 'api';
