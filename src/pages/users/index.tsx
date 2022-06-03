5378d3febf97e0ba7b83985d2e021c3bbc661359
Giovanni Crabi <crabigiovanni@gmail.com>
2022-06-03 23:32:19 +0100

    Problem with Paginations

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,6 +1,6 @@
 import { Box, Button, Checkbox, Flex, Heading, Icon, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
 import Link from "next/link";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { RiAddLine} from "react-icons/ri";
 
 import { Header } from "../../components/Header";
@@ -9,7 +9,9 @@ import { SideBar } from "../../components/Sidebar";
 import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UserList() {
-  const { data, isLoading, error, isFetching } = useUsers();
+  const [page, setPage] = useState(1);
+
+  const { data, isLoading, error, isFetching } = useUsers(page);
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -70,7 +72,7 @@ export default function UserList() {
                 </Tr>
               </Thead>
               <Tbody>
-                {data.map(user => {
+                {data.users.map(user => {
                   return (
                       <Tr key={user.id}>
                         <Td px={['4', '4', '6']}>
@@ -90,11 +92,11 @@ export default function UserList() {
             </Table>
           )}
 
-          <Pagination  
-            totalCountOfRegisters={200}
-            currentPage={5}  
-            onPageChange={() => {}}
-          />
+              <Pagination 
+                totalCountOfRegisters={data.totalCount}
+                currentPage={page}
+                onPageChange={setPage}
+              />
 
         </Box>
       </Flex>
--- src/services/hooks/useUsers.ts
+++ src/services/hooks/useUsers.ts
@@ -1,34 +1,49 @@
 import { useQuery } from "react-query";
+
 import { api } from "../api";
 
-type User = { 
+type User = {
   id: string;
   name: string;
   email: string;
-  created_at: string;
+  created_at: string
 }
 
-export async function getUsers(): Promise<User[]> {
-    const { data } = await api.get('users/')
+type GetUsersResponse = {
+  totalCount: number;
+  users: User[];
+}
 
-    const users = data.users.map(user => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        created_at: new Date(user.created_at).toLocaleDateString('pt-br', {
-          day: '2-digit',
-          month:'long',
-          year: 'numeric'
-        })
-      }
-    });
-    return users;
-  }
+export async function getUsers(page: number): Promise<GetUsersResponse> {
+  const { data, headers } = await api.get('users', {
+    params: {
+      page,
+    }
+  });
 
+  const totalCount = Number(headers['x-total-count'])
 
-export function useUsers() {
-  return useQuery('users', getUsers, {
-    staleTime: 1000 * 5 // 5 segundos 
+  const users = data.users.map(user => {
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      }),
+    };
+  });
+
+  return {
+    users,
+    totalCount
+  }; 
+}
+
+export  function useUsers(page: number) {
+  return useQuery('users', () => getUsers(page), {
+    staleTime: 1000 * 60 * 10,
   });
 }
